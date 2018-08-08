baecaec33e2c86d6a65448fef829c2ac7af209e2
brilloueta <alexandre_brillouet@conair.com>
2018-08-08 13:38:14 +0200

    add json file and mock_gpio into the folder

---
+++ mock_gpio.py
@@ -0,0 +1,21 @@
+BCM = None
+HIGH = None
+LOW = None
+IN = None
+OUT = None
+PUD_UP = None
+
+def input(*args, **kwargs):
+    pass
+
+def output(*args, **kwargs):
+    pass
+
+def setmode(*args, **kwargs):
+    pass
+
+def setwarnings(*args, **kwargs):
+    pass
+
+def setup(*args, **kwargs):
+    pass
