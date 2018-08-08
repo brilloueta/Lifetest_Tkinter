0d8ed365799fdec773e857b204e29267a00429f0
brilloueta <alexandre_brillouet@conair.com>
2018-08-08 13:39:28 +0200

    modif variable counter_value in counter_val + add some comments for future structure

--- Lifetest_ Tk_Interface.py
+++ Lifetest_ Tk_Interface.py
@@ -1,21 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
-from tkinter import *
 
-class App:
-    def __init__(self, master):
+# bibliothèques
+try:
+    import RPi.GPIO as GPIO
+except ImportError:
+    print('LOADING MOCK GPIO')
+    import mock_gpio as GPIO
 
-        #Valeur compteurs par défaut
-        global counter_value
-        counter_value = int() #faut il utiliser une variable globale? comment ?
-        counter_value = 5234
-        
-        qty_cycles=96
-        timer_ON=300
-        timer_OFF=300
-        timer_REST=300
-        timer_CHARGE=300
+import argparse
+import json
+import pathlib
+import sys
+import time
+from tkinter import*
+
+
+#Valeur états de l'appli par défaut
+
+DEFAULT_APP_STATE = {
+    'cycles': 0,
+    'date': None,
+    'pas_incr': 1,
+    'counter_val': 1,
+    'qty_cycles': 96,
+    'timer_ON': 300,
+    'timer_OFF': 300,
+    'timer_REST': 300,
+    'timer_CHARGE': 300
+}
+      
+
+#Déposer et charger les états de l'appli
+
+def dump_app_state(file_path, **kwargs):
+    state = load_app_state(file_path)
+    state.update(kwargs)
+    state['date'] = date_sec_epoch()
+
+    with open(file_path, 'w') as fd:
+        json_str = json.dumps(state)
+        fd.write(json_str)
+
+
+def load_app_state(file_path):
+    path = pathlib.Path(file_path)
+    if not path.exists():
+        return DEFAULT_APP_STATE
+
+    with path.open('r') as fd:
+        json_str = fd.read()
+        d = json.loads(json_str)
+        return d
+
+
+
+#Valeur interface Tkinter par défaut
+
+
+
+
+#Déposer et charger les valeurs interface Tkinter 
+
+
+
+
+
+#Prévoir une classe pour l'interface Tkinter
+    #Sur appui départ cycle => lancer déroulement du programme
+
+#Prévoir une classe pour le déroulement du programme
+    
+class App(object):
+    def __init__(self, log_file, app_state_file):
+        self.log_file = log_file
+        self.app_state_file = app_state_file
 
         #Dimensions des cadres
         Wf=900
@@ -86,8 +145,6 @@ class App:
         fakeline7.grid(row=7, column=0)
         fakeline8=Label(cadre1B, text="", bg="brown")
         fakeline8.grid(row=8, column=0)
-
-
         
         label1=Label(cadre1B, text = "Counter initial value")
         label1.grid(row=1, column=1,padx=10, pady=5, sticky=W)
@@ -127,7 +184,7 @@ class App:
         button6M= Button(cadre1B, text="-")
         button6M.grid (row=8, column =3,padx=10, pady=5)
 
-        value1=Label(cadre1B, textvariable = str(counter_value))
+        value1=Label(cadre1B, textvariable = str(counter_val))
         value1.grid(row=1, column=4,padx=10, pady=5, sticky=E)
         value2=Label(cadre1B, text = qty_cycles)
         value2.grid(row=3, column=4,padx=10, pady=5, sticky=E)
@@ -139,17 +196,218 @@ class App:
         value5.grid(row=7, column=4,padx=10, pady=5, sticky=E)
         value6=Label(cadre1B, text = timer_CHARGE)
         value6.grid(row=8, column=4,padx=10, pady=5, sticky=E)
-        
+
     def say_hi(self):
         print ("hi there, everyone!")
 
-
     def button1P_action (self):
-        counter_value += 1
-        value1.config(text=str(counter_value))
+        counter_val += 1
+        value1.config(text=str(counter_val))
         value1.grid(row=1, column=4,padx=10, pady=5, sticky=E)
 
-root = Tk()
-app = App(root)
-root.mainloop()
+    def get_cycles(self):
+        return load_app_state(self.app_state_file)['cycles']
 
+    def set_cycles(self, cycle):
+        dump_app_state(self.app_state_file, cycles=cycle)
+
+    def get_pas_incr(self):
+        return load_app_state(self.app_state_file)['pas_incr']
+
+    def set_pas_incr(self, pas_incr):
+        dump_app_state(self.app_state_file, pas_incr=pas_incr)
+
+    def get_counter_val(self):
+        return load_app_state(self.app_state_file)['counter_val']
+
+    def set_counter_val(self, pas_incr):
+        dump_app_state(self.app_state_file, counter_val=counter_val)
+
+    def get_qty_cycles(self):
+        return load_app_state(self.app_state_file)['qty_cycles']
+
+    def set_qty_cycles(self, pas_incr):
+        dump_app_state(self.app_state_file, qty_cycles=qty_cycles)
+
+    def get_timer_ON(self):
+        return load_app_state(self.app_state_file)['timer_ON']
+
+    def set_timer_ON(self, pas_incr):
+        dump_app_state(self.app_state_file, timer_ON=timer_ON)
+        
+    def get_timer_OFF(self):
+        return load_app_state(self.app_state_file)['timer_OFF']
+
+    def set_timer_OFF(self, pas_incr):
+        dump_app_state(self.app_state_file, timer_OFF=timer_OFF)
+        
+    def get_timer_REST(self):
+        return load_app_state(self.app_state_file)['timer_REST']
+
+    def set_timer_REST(self, pas_incr):
+        dump_app_state(self.app_state_file, timer_REST=timer_REST)
+        
+    def get_timer_CHARGE(self):
+        return load_app_state(self.app_state_file)['timer_CHARGE']
+
+    def set_timer_CHARGE(self, pas_incr):
+        dump_app_state(self.app_state_file, timer_CHARGE=timer_CHARGE)
+
+    def log_to_csv(self, cycles):
+        log_to_csv(self.log_file, cycles)
+
+
+def date_sec_epoch():
+    #return time.strftime('%d/%m/%y %H:%M:%S', time.localtime())
+    return time.time()
+
+def log_time():
+    return time.strftime('%d/%m/%y %H:%M:%S', time.localtime(date_sec_epoch()))
+
+def arduino_idle():
+    GPIO.output(25,GPIO.LOW)
+
+
+def pause_board(msg, delay=15):
+    print(msg, time.strftime('%d/%m/%y %H:%M:%S', time.localtime(date_sec_epoch())))
+    arduino_idle()
+    time.sleep(delay)
+
+
+def log_to_csv(file_path, cycles):
+    with open(file_path, "a") as fd:
+        fd.write(str(date_sec_epoch()))
+        fd.write(", ")
+        fd.write(log_time())
+        fd.write(", ")
+        fd.write(str(cycles))
+        fd.write("\n")
+
+def init_cycles(app):
+    cycles_string = app.get_cycles()
+    print ('La derniere valeur enregistree du compteur est {}'.format(cycles_string))
+    choix = input('Souhaitez vous modifier la valeur ? Y/N \n')
+
+    if choix.lower() in ['y', 'yes']:
+        cycles_string = input('\nSaisir la nouvelle valeur pour le compteur \n')
+        print ('\nLa nouvelle valeur du compteur est {} \n'.format(cycles_string))
+        app.set_cycles(int(cycles_string))        # dump de l'etat courant
+
+    elif choix.lower() in ['n', 'no']:
+        print ('\nLa valeur enregistree du compteur est {} \n'.format(cycles_string))
+        cycle = int(cycles_string)
+
+    else:
+        print ("Le choix n'est pas repertorie, le prog s'arrete ici ! \n \n")
+        sys.exit(0)
+
+
+def init_pas_incr(app):
+    pas_incr = app.get_pas_incr()
+    print ('chaque fin de boucle ajoute {} cycles de 5min ON'.format(pas_incr))
+    choix = input('Souhaitez vous modifier la valeur ? Y/N \n')
+
+    if choix.lower() in ['y', 'yes']:
+        pas_incr = input('\nSaisir la nouvelle valeur de pas \n')
+        print ('\nchaque fin de boucle ajoute {} cycles de 5min ON \n'.format(pas_incr))
+        app.set_pas_incr(int(pas_incr))        # dump de l'etat courant
+
+    elif choix.lower() in ['n', 'no']:
+        print ('\nchaque fin de boucle ajoute dorénavant {} cycles de 5min ON \n'.format(pas_incr))
+        pas_incr = int(pas_incr)
+
+    else:
+        print ("Le choix n'est pas repertorie, le prog s'arrete ici ! \n \n")
+        sys.exit(0)
+
+def main(args):
+
+    root = Tk()
+    app = App(root)
+    root.mainloop()
+
+    app = App(
+        log_file=args.log_file,
+        app_state_file=args.app_state_file
+    )
+    # configurations
+    GPIO.setmode(GPIO.BCM)                      # mode de numérotation des pins
+    GPIO.setwarnings(False)                     # supprime les messages d'erreurs
+
+    # declaration des I/O
+    GPIO.setup(25,GPIO.OUT, initial=GPIO.LOW)           # la pin 25 réglee en sortie, valeur basse par défaut => départ cycle ARDUINO
+    GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # la pin 4 réglée en entrée => bouton bascule programme actif sur raspberry
+    GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_UP)   # la pin 19 réglée en entrée => signal haut envoyé par arduino en fin de programme arduino
+
+    # variables perso
+    init_cycles(app)
+    init_pas_incr(app)
+    
+    # Verifie les conditions pour lancer le prog arduino
+    while True:
+
+        fin_arduino = GPIO.input(19)
+        bouton_bascule = GPIO.input(4)
+
+        current_day = time.strftime('%A',time.localtime(date_sec_epoch()))
+        current_hour = int(time.strftime('%H',time.localtime(date_sec_epoch())))
+
+        # tant que l'arduino n'a pas termine son programme continue ce qui suit
+        if fin_arduino == 0:
+            print ("cycles ON/OFF terminé", log_time())
+            
+            # verifie que le bouton bascule raspberry est active
+            if bouton_bascule == 0:
+                print ("selecteur en position ON", log_time())
+            
+                # condition if, si jour = jour de semaine samedi ou dimanche alors demarrer, sinon attendre 15 minutes
+                if current_day.lower() in ["monday", "tuesday", "wednesday", "thursday", "friday"]:
+                    print ("programme dans les jours fixes", log_time())
+                
+                    # condition if, si heure >8H et <17H alors demarrer, sinon attendre 15 minutes
+                    if current_hour >= 8 and  current_hour <= 17:
+                        print ("programme dans les heures fixes", log_time())
+
+                        #depart cycle
+                        GPIO.output(25,GPIO.HIGH)       # depart cycle pour l'arduino
+
+                        #affichage dans le shell
+                        print ("depart programme le ", log_time(), "\n")
+
+                        old_cycle = cycle
+                        cycle += pas_incr
+                        print('Le compteur passe de {} à {}'.format(old_cycle, cycle))
+
+                        app.set_cycles(cycle)        # dump de l'etat courant
+                        app.log_to_csv(cycle)
+
+                        time.sleep(50)              # on ne change rien pendant X minutes, initalement 1 minute (tps supérieur au déroulement du prog arduino)
+
+                    else:
+                        pause_board("programme en dehors des heures fixes")
+
+                else:
+                    pause_board("programme en dehors des jours fixes")
+
+            else:
+                pause_board("selecteur en position OFF")
+
+        else:
+            pause_board("cycles ON/OFF déja en cours")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--log-file',
+                        default='Log_Lifetest.csv',
+                        help="Path to the log file (CSV)")
+    parser.add_argument('-a', '--app-state-file',
+                        default='app_state.json',
+                        help="Path to the app state file (JSON)")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main(get_args())
+
+     
